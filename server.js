fed3f5da3f89d999b75374e64d244339e607d052
andan <andante1104@naver.com>
2023-12-23 19:00:27 +0900

    express 서버 정적파일 주고받는 미들웨어  생성

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!')
+
+    res.sendFile('index.html' , {root: path.join(__dirname,'public')});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
